d939c98c34c58c37cfe490eaea01bc6cbb370f88
Celeste De Santiago <celestedaphne@gmail.com>
2017-10-17 17:26:35 -0700

    Almost works, currently prints out all results on page, not just under Title

---
+++ movie-search.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const cheerio = require('cheerio')
+
+const searchTerm = process.argv.slice(2).join(' ').toLowerCase().trim()
+  .replace(/[^\w\s\d]/g, '')
+  .replace(/\s+/g, '+')
+
+const options = {
+  hostname: 'www.imdb.com',
+  path: `/find?ref_=nv_sr_fn&q=${searchTerm}&s=all`,
+}
+const callback = (response) => {
+  let str = ''
+
+  // another chunk of data has been recieved, so append it to `str`
+  response.on('data', (chunk) => {
+    str += chunk
+  })
+
+  // the whole response has been recieved, so we just print it out here
+  response.on('end', () => {
+    const $ = cheerio.load(str)
+
+    //console.log($('.findList').attr('id', 'Titles').html())
+    let theText = $('.result_text').text()
+    $('.result_text').children('small').each((ind, elem) => {
+      theText = theText.replace($(elem).text(), '')
+    })
+    console.log(insertBreaks(theText.trim()))
+  })
+}
+
+http.request(options, callback).end()
+
+/**
+* Inserts line breaks between each film
+* @param {string} movieTitlesString
+* @returns {string}
+*/
+const insertBreaks = (movieTitlesString) => {
+  const betweenEach = /\)\s+([^(\s])/g
+  return movieTitlesString.replace(betweenEach, ')\n$1')
+}
