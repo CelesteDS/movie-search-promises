cc42545252848a12a35df1e3cdda98425ddba9ac
Celeste De Santiago <celestedaphne@gmail.com>
2017-10-19 11:24:46 -0700

    Now removes extra details about tv series etc

--- movie-search.js
+++ movie-search.js
@@ -20,9 +20,13 @@ const callback = (response) => {
   // the whole response has been recieved, so we just print it out here
   response.on('end', () => {
     const $ = cheerio.load(str)
+    let theText
 
-    //console.log($('.findList').attr('id', 'Titles').html())
-    let theText = $('.result_text').text()
+    $('.findList').each((i, elem) => {
+      theText += `how many times ${$('.result_text', elem).text()}`
+      return false
+    })
+
     $('.result_text').children('small').each((ind, elem) => {
       theText = theText.replace($(elem).text(), '')
     })
